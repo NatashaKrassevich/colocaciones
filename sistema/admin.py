7e4b397fe6122ce11335ccd07ba93a48df0e48fe
Akka <akka47@tfwno.gf>
2017-09-14 09:12:43 -0300

    cambios en el admin.py

--- sistema/admin.py
+++ sistema/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Agencia
+from .models import Persona
+from .models import Empresa
+from .models import RegistroDeEmpleados
+from .models import OfertaDeTrabajo
+from .models import TipoDeTrabajo
 
 # Register your models here.
+
+admin.site.register(Agencia)
