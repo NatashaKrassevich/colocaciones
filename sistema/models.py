5abd0946ef0cb4419a60fb580c586e08465db5c3
NatashaKrassevich <naaaaati.belen@gmail.com>
2017-09-14 08:40:10 -0300

    Cambios en models.py

--- sistema/models.py
+++ sistema/models.py
@@ -3,17 +3,10 @@ from django.db import models
 from django.utils import timezone
 
 class Agencia(models.Model):
-    agencia = 
     telefono = models.CharField(max_length=200) 
     direccion = models.CharField(max_length=200)
-    email = models.EmailField(max_length=200)
-
-    def __str__(self):
-        return self.
-
-    def 
-            
-
+    email = models.EmailField(max_length=200)   
+   
 class Persona(models.Model):
     nombre = models.CharField(max_length=200)
     apellido = models.CharField(max_length=200)
