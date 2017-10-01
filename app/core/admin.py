038cdf22d90af435b7b765c53c33b6ac5365e162
Alan Rodas Bonjour <alanrodas@gmail.com>
2017-10-01 17:09:09 -0300

    Eliminado entorno, cache y agregado registro

---
+++ app/core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Desocupado)
+admin.site.register(Empresa)
+admin.site.register(RegistroDeEmpleados)
+admin.site.register(OfertaDeTrabajo)
+admin.site.register(TipoDeTrabajo)
